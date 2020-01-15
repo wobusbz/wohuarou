9ab1a0a2225a4de03d1f88ec6de6bdef5269e5b7
wobusbz <1135455278@qq.com>
2020-01-16 01:01:43 +0800

    添加单元测试

---
+++ util/util_test.go
@@ -0,0 +1,27 @@
+package util
+
+import (
+	"net/http"
+	"testing"
+)
+
+var httpUnitTest = []struct {
+	title string
+	f     func(string) (*http.Response, error)
+	out   string
+}{
+	{"TestHTTPGet", HTTPGet, "200"},
+}
+
+func TestHTTPGet(t *testing.T) {
+	var url = "http://www.baidu.com"
+	resp, err := HTTPGet(url)
+	if resp.Status != "200" && err != nil {
+		t.Errorf("reality: %s  expect:%d", resp.Status, 200)
+	}
+	for _, v := range httpUnitTest {
+		if resp, err := v.f(url); resp.Status != v.out && err != nil {
+			t.Errorf("reality: %s expect: %s", resp.Status, v.out)
+		}
+	}
+}
